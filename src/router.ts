253819e188155f17889a491d2a32cffc8884895f
adnan <adnan.mohammad@scrapehero.com>
2022-12-25 16:43:51 +0530

    init router setup and nodemon

---
+++ src/router.ts
@@ -0,0 +1,36 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * Product
+ */
+router.get("/product", (req, res) => {
+  res.json({
+    message: "Take this list of products",
+  });
+});
+router.get("/product/:id", () => {});
+router.put("/product/:id", () => {});
+router.post("/product", () => {});
+router.delete("/product/:id", () => {});
+
+/**
+ * Update
+ */
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put("/update/:id", () => {});
+router.post("/update", () => {});
+router.delete("/update/:id", () => {});
+
+/**
+ * Update Point
+ */
+router.get("/update-point", () => {});
+router.get("/update-point/:id", () => {});
+router.put("/update-point/:id", () => {});
+router.post("/update-point", () => {});
+router.delete("/update-point/:id", () => {});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 const app = express();
 
@@ -7,5 +8,7 @@ app.get("/", (req, res) => {
   res.status(200);
   res.sendFile(path.resolve("pages/index.html"));
 });
+
+app.use("/api", router);
 
 export default app;
