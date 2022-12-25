596233410665f9bada7efbe565026acc285c355e
adnan <adnan.mohammad@scrapehero.com>
2022-12-25 16:51:04 +0530

    setup middlewares morgan, cors, json and urlencoded

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200);
